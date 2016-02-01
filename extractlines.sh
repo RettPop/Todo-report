ea04a3f9eb5403bc0c47d31b017a04abf0bb9a02
Vyacheslav Zhelobkov <vyacheslav.zhelobkov@playtech.com>
2016-02-01 17:35:06 +0200

    Parameters fix. Temp file is used now

--- extractlines.sh
+++ extractlines.sh
@@ -3,7 +3,7 @@
 # check if no params
 if [[ -z $6 ]]; then
 	echo "Report wanted text (TODO) from files under the current directory to specified file."
-	echo "Wrong or absent parameters. Usage:\n$0 -t|--text <text regexp to look for> -m|--filemask <files mask> -o|--outfile <write result to name>"
+	echo "Wrong or absent parameters. Usage:\n$0 -t|--text <text regexp to look for> -m|--filemask <files mask> -o|--outfile <write result to name> -d|--rootdir <search files in>"
 	exit 2 
 fi
 
@@ -13,18 +13,22 @@ do
 		-t|--text)
 			LOOKFORTEXT="$2"
 			shift # past argument=value
+			shift # past argument=value
 			;;
 		-m|--filemask)
 			INFILESMASK="$2"
+			shift # past argument=value
 			shift # past argument=value
 			;;
 		-o|--outfile)
 			OUTFILE="$2"
 			shift # past argument=value
+			shift # past argument=value
 			;;
-		--default)
-			DEFAULT=YES
-			shift # past argument with no value
+		-d|--rootdir)
+			PARENTDIR="$2"
+			shift # past argument=value
+			shift # past argument=value
 			;;
 		*)
 			# unknown option
@@ -34,14 +38,17 @@ done
 
 FILESCOUNT=0
 LINESSCOUNT=0
-#LOOKFORTEXT=$2
-#OUTFILE=$1
+TMPFILE="/tmp/$(basename $0).$$.tmp"
+#PARENTDIR=$2
+#LOOKFORTEXT=$1
+#OUTFILE=$4
 #INFILESMASK=$3
 echo "$LOOKFORTEXT"
 echo "$OUTFILE"
 echo "$INFILESMASK"
-FILES=$(find ./ -name $INFILESMASK -exec grep -l $LOOKFORTEXT {} \;)
-echo "" > $OUTFILE 
+echo "$PARENTDIR"
+
+FILES=$(find $PARENTDIR -name $INFILESMASK -exec grep -l $LOOKFORTEXT {} \;)
 
 for f in $FILES
 do
@@ -50,15 +57,18 @@ do
 	((LINESCOUNT+=$(grep $LOOKFORTEXT $f | wc -l)))
 
 	#
-	echo "===============================================" >> $OUTFILE
-	echo "File: $f" >> $OUTFILE
-	echo "===============================================" >> $OUTFILE
-	grep -C 3 -n $LOOKFORTEXT $f >> $OUTFILE
+	echo "===============================================" >> $TMPFILE
+	echo "File: $f" >> $TMPFILE
+	echo "===============================================" >> $TMPFILE
+	grep -C 3 -n $LOOKFORTEXT $f >> $TMPFILE
 done
 
-echo "Lines found $LINESCOUNT\n$(cat $OUTFILE)" > $OUTFILE
-echo "Files found $FILESCOUNT\n$(cat $OUTFILE)" > $OUTFILE
-echo "\n$(cat $OUTFILE)" > $OUTFILE
+echo "Lines found $LINESCOUNT\n$(cat $TMPFILE)" > $TMPFILE
+echo "Files found $FILESCOUNT\n$(cat $TMPFILE)" > $TMPFILE
+echo "\n$(cat $TMPFILE)" > $TMPFILE
+
+cat $TMPFILE > $OUTFILE
+rm $TMPFILE
 
 echo "Files found $FILESCOUNT"
 echo "Lines found $LINESCOUNT"
